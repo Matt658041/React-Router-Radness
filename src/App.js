11cb9aa573db72ac0b36d7acd9b03e2cadae8323
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-10-05 14:21:09 -0500

    added new quotes files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from "react";
+import {Route, Switch} from 'react-router-dom'
+
 function App() {
   return (
-    <div>
+    <Switch>
+      <Route path='/quotes'>
 
-    </div>
+      </Route>
+      <Route path='/quotes/:quoteId'>
+
+      </Route>
+      <Route path='/new-quote'>
+
+      </Route>
+   </Switch>
   );
 }
 
---
+++ src/components/pages/AllQuotes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllQuotes = () => {
+  return (
+    <div>AllQuotes</div>
+  )
+}
+
+export default AllQuotes

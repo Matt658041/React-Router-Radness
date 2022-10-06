d22a7258245ee244f8dc46b3cdec8da14000fc75
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-10-06 13:07:48 -0500

    added not found

--- src/components/pages/QuoteDetail.jsx
+++ src/components/pages/QuoteDetail.jsx
@@ -1,13 +1,25 @@
 import React, { Fragment } from 'react'
 import { Route, useParams } from 'react-router-dom'
 import Comments from '../comments/Comments';
+import HighlightedQuote from '../quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "learning react is fun" },
+  { id: "q2", author: "MaxxyPad", text: "learning react is good" },
+];
 
 const QuoteDetail = () => {
   const params = useParams()
+
+  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author } />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments/>
       </Route>
